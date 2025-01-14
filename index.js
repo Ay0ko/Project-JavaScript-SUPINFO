e5156c316bf549f5245759dcf020b0b10c4237b7
Aiqing <thomas.martin@supinfo.com>
2025-01-14 19:40:03 +0100

    Ajout video/logo + prettier

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-const nav = document.querySelectorAll('.nav .navigation');
+const nav = document.querySelectorAll(".nav .navigation");
 
-nav.forEach(link => {
-    link.addEventListener('mouseover', () => {
-        link.style.textDecoration = 'underline'; 
-    });
+nav.forEach((link) => {
+  link.addEventListener("mouseover", () => {
+    link.style.textDecoration = "underline";
+  });
 
-    link.addEventListener('mouseout', () => {
-        link.style.textDecoration = 'none'; 
-    });
+  link.addEventListener("mouseout", () => {
+    link.style.textDecoration = "none";
+  });
 });
