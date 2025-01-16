74d34affda794c18ed8bc5b1e6400acbe9e945b6
Aiqing <thomas.martin@supinfo.com>
2025-01-16 09:37:35 +0100

    Update index.js

--- index.js
+++ index.js
@@ -9,3 +9,16 @@ nav.forEach((link) => {
     link.style.textDecoration = "none";
   });
 });
+
+
+const filmTrend = [ "Guardians of the Galaxy Vol. 3",
+                    "Mufasa : The Lion King",
+                    "Un ours dans le Jura",
+                    "Beating Hearts",
+                    "Gladiator II",
+                    "Venom the Last Dance",
+                    "The Most Precious of Cargoes",
+                    "BabyGirl",
+                    "Monsieur Aznavour"
+                  ];
+
