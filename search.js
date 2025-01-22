8187d3ef1e7f52aece8a20162b81e326e7d0efb1
Aiqing <thomas.martin@supinfo.com>
2025-01-22 13:52:16 +0100

    Ajout img.png + correction de search

--- search.js
+++ search.js
@@ -1,100 +1,100 @@
-// clé : 1d817c6c
+// clé : 190fc5fe
 // titre : https://www.omdbapi.com/?s=thr&page=1&apikey=1d817c6c
 // details : https://www.omdbapi.com/?i=tt3896198&apikey=1d817c6c
 
-// const inputRecherche = document.getElementById('search');
-// const retourRecherche = document.getElementById('ResultatRecherche');
+const inputRecherche = document.getElementById('search');
+const retourRecherche = document.getElementById('ResultatRecherche');
 
 
-// async function chargerFilms(elementEntre) {
-//   const URL = `https://www.omdbapi.com/?s=${elementEntre}&page=1&apikey=1d817c6c`;
-//   const recherche = await fetch(`${URL}`);
-//   const donnees = await recherche.json;
-//   if (donnees == "True") chargerFilms(donnees.recherche);
-// }
+async function chargerFilms(elementEntre) {
+  const URL = `${"https://www.omdbapi.com/"}?apikey=${"190fc5fe"}&s=${movieTitle}`;
+  const recherche = await fetch(`${URL}`);
+  const donnees = await recherche.json();
+  if (donnees == "True") chargerFilms(donnees.recherche);
+}
 
-// function chercherFilms() {
-//   let entree = (inputRecherche.value).trrim();
-//   if (entree.length > 0) {
-//     entree = 0;
-//   }
-// }
+function chercherFilms() {
+  let entree = (inputRecherche.value).trim();
+  if (entree.length > 0) {
+    entree = 0;
+  }
+}
 
-// function afficherFilms(films) {
-//   retourRecherche.innerHTML = "";
-//   for (let idx = 0; idx < films.length; idx++) {
-//     let afficheFilm = document.createElement('div');
-//     afficheFilm.dataset.id = films[idx].imdbID;
-//     afficheFilm.classList.add('listeElementsRecherche');
-//     if (films[idx].Poster != "N/A")
-//       posterFilm = films[idx].Poster;
-//     else
-//       posterFilm = "image_non_trouvee.png"
-//     afficheFilm.innerHTML = `
-//     <div class="poster">
-//       <img src="${posterFilm}" alt="Affiche du film : ${films[idx].Title}">
-//     </div>
-//       <div class="infoElementsRecherche">
-//           <h3> ${films[idx].Title} </h3>
-//           <p> ${films[idx].Released} </p>
-//       </div>
-//     </div>`;
-//     retourRecherche.appendChild(afficheFilm);
-//   }
-// }
+function afficherFilms(films) {
+  retourRecherche.innerHTML = "";
+  for (let idx = 0; idx < films.length; idx++) {
+    let afficheFilm = document.createElement('div');
+    afficheFilm.dataset.id = films[idx].imdbID;
+    afficheFilm.classList.add('listeElementsRecherche');
+    if (films[idx].Poster != "N/A")
+      posterFilm = films[idx].Poster;
+    else
+      posterFilm = "img/image_non_trouvee.png"
+    afficheFilm.innerHTML = `
+    <div class="poster">
+      <img src="${posterFilm}" alt="Affiche du film : ${films[idx].Title}">
+    </div>
+      <div class="infoElementsRecherche">
+          <h3> ${films[idx].Title} </h3>
+          <p> ${films[idx].Released} </p>
+      </div>
+    </div>`;
+    retourRecherche.appendChild(afficheFilm);
+  }
+}
 
-document.addEventListener("DOMContentLoaded", () => {
-  const entreeRecherche = document.getElementById("search");
-  const ResultatRecherche = document.getElementById("ResultatRecherche");
-  const creerAffiche = (film) => {
-    const affiche = document.createElement("img");
-    affiche.src = film.image;
-    affiche.alt = `Chargement de l'affiche du film : '${film.title}'`;
-    affiche.className = "afficheChargee";
-    affiche.addEventListener("click", () => {
-      const params = new URLSearchParams({
-        title: film.title,
-        image: film.image,
-        imdb: film.imdb,
-      });
-      window.location.href = `movie.html?${params.toString()}`;
-    });
-    ResultatRecherche.appendChild(affiche);
-  };
-  const chercherFilm = async (titre) => {
-    if (!titre) {
-      ResultatRecherche.innerHTML = "";
-      return;
-    }
-    try {
-      const reponse = await fetch(
-        `${"https://www.omdbapi.com/"}?apikey=${"190fc5fe"}&s=${titre}`
-      );
-      const data = await reponse.json();
-      ResultatRecherche.innerHTML = "";
-      if (data.Reponse == "True") {
-        data.Search.forEach((film) => {
-          const afficheChargee = creerAffiche(film);
-          ResultatRecherche.appendChild(afficheChargee);
-        });
-      } else {
-        ResultatRecherche.innerHTML =
-          "Nous n'avons pas trouvé de film correspondant à votre recherche.";
-      }
-    } catch (erreur) {
-      console.error(
-        "Il y a eu un problème lors de la récupéation des films : ",
-        erreur
-      );
-      ResultatRecherche.innerHTML =
-        "Une erreur est survenue lors de la récupération des films, veuillez réessayer.";
-    }
-  };
-  entreeRecherche.addEventListener("input", (event) => {
-    const titre = event.target.value.trim();
-    chercherFilm(titre);
-  });
-});
+// document.addEventListener("DOMContentLoaded", () => {
+//   const entreeRecherche = document.getElementById("search");
+//   const ResultatRecherche = document.getElementById("ResultatRecherche");
+//   const creerAffiche = (film) => {
+//     const affiche = document.createElement("img");
+//     affiche.src = film.image;
+//     affiche.alt = `Chargement de l'affiche du film : '${film.title}'`;
+//     affiche.className = "afficheChargee";
+//     affiche.addEventListener("click", () => {
+//       const params = new URLSearchParams({
+//         title: film.title,
+//         image: film.image,
+//         imdb: film.imdb,
+//       });
+//       window.location.href = `movie.html?${params.toString()}`;
+//     });
+//     ResultatRecherche.appendChild(affiche);
+//   };
+//   const chercherFilm = async (titre) => {
+//     if (!titre) {
+//       ResultatRecherche.innerHTML = "";
+//       return;
+//     }
+//     try {
+//       const reponse = await fetch(
+//         `${"https://www.omdbapi.com/"}?apikey=${"190fc5fe"}&s=${titre}`
+//       );
+//       const data = await reponse.json();
+//       ResultatRecherche.innerHTML = "";
+//       if (data.Reponse == "True") {
+//         data.Search.forEach((film) => {
+//           const afficheChargee = creerAffiche(film);
+//           ResultatRecherche.appendChild(afficheChargee);
+//         });
+//       } else {
+//         ResultatRecherche.innerHTML =
+//           "Nous n'avons pas trouvé de film correspondant à votre recherche.";
+//       }
+//     } catch (erreur) {
+//       console.error(
+//         "Il y a eu un problème lors de la récupéation des films : ",
+//         erreur
+//       );
+//       ResultatRecherche.innerHTML =
+//         "Une erreur est survenue lors de la récupération des films, veuillez réessayer.";
+//     }
+//   };
+//   entreeRecherche.addEventListener("input", (event) => {
+//     const titre = event.target.value.trim();
+//     chercherFilm(titre);
+//   });
+// });
 
 
 // const searchInput = document.getElementById("search");
