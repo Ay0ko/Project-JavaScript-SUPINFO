4919b1e056cd1902fa5241bcfe40c749daae6f1c
Aiqing <thomas.martin@supinfo.com>
2025-01-22 15:26:38 +0100

    Prettier

--- movie.js
+++ movie.js
@@ -6,32 +6,38 @@ document.addEventListener("DOMContentLoaded", async () => {
     return;
   }
 
-  console.log("Données du film sélectionné:", selectedFilm); 
+  console.log("Données du film sélectionné:", selectedFilm);
 
   const filmPoster = document.getElementById("filmPoster");
   filmPoster.src = selectedFilm.image;
   filmPoster.alt = `Affiche du film : ${selectedFilm.title}`;
-  
+
   const filmTitle = document.getElementById("filmTitle");
   filmTitle.textContent = selectedFilm.title;
 
   try {
-    const response = await fetch(`https://www.omdbapi.com/?i=${selectedFilm.imdb}&plot=full&apikey=${"190fc5fe"}`);
+    const response = await fetch(
+      `https://www.omdbapi.com/?i=${
+        selectedFilm.imdb
+      }&plot=full&apikey=${"190fc5fe"}`
+    );
     const movieData = await response.json();
-  
+
     if (movieData.Response === "True") {
       const actorNames = document.getElementById("actorNames");
       actorNames.textContent = movieData.Actors || "N/A";
-  
+
       const directorName = document.getElementById("directorName");
       directorName.textContent = movieData.Director || "N/A";
 
       const genreName = document.getElementById("genreName");
       genreName.textContent = movieData.Genre || "N/A";
-  
+
       const plotElement = document.createElement("p");
       plotElement.className = "movie-plot";
-      plotElement.innerHTML = `<strong>Resume :</strong> ${movieData.Plot || "Résumé non disponible."}`;
+      plotElement.innerHTML = `<strong>Resume :</strong> ${
+        movieData.Plot || "Résumé non disponible."
+      }`;
       document.querySelector(".movie-details").appendChild(plotElement);
     } else {
       console.error("Erreur API OMDb :", movieData.Error);
@@ -39,6 +45,8 @@ document.addEventListener("DOMContentLoaded", async () => {
     }
   } catch (error) {
     console.error("Erreur de requête API :", error);
-    alert("Une erreur est survenue lors de la récupération des détails du film.");
+    alert(
+      "Une erreur est survenue lors de la récupération des détails du film."
+    );
   }
-});  
+});
